4625e82fe6476665333b6770bb1057b83a43306c
Krunal Patel <Pkrunal616@gmail.com>
2019-11-06 20:50:40 -0500

    action + reducer are set up

---
+++ src/action/index.js
@@ -0,0 +1,15 @@
+export const ADD_ITEM = 'ADD_ITEM'
+
+export const addIteam = () => {
+    return{
+        type: ADD_ITEM
+    }
+}
+
+export const REMOVE_ITEM = 'REMOVE_ITEM'
+
+export const removeIteam = () => {
+    return{
+        type: REMOVE_ITEM
+    }
+}
